c3655d3981716a805a68c85ebfc358299ebb72d0
youssefberrk <yberrakouan.lol@gmail.com>
2026-02-22 15:00:04 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { CircleAlertIcon } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+	return (
+    <div className="">
+
+		
+    </div>
+	);
+}
