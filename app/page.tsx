f6d05d4bd593b97ed794b437978bba620679a433
youssefberrk <yberrakouan.lol@gmail.com>
2026-02-28 16:24:31 +0000

    seconde commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import { Button } from "@/components/ui/button";
-import { CircleAlertIcon } from "lucide-react";
-import Image from "next/image";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
+import AppAreaChart from "@/components/AppAreaChart";
+import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 
-export default function Home() {
+const Homepage = () => {
 	return (
-    <div className="">
-
-		
-    </div>
+		<div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+			<div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+				<AppBarChart />
+			</div>
+			<div className="bg-primary-foreground p-4 rounded-lg">
+				<CardList title="Latest Transactions" />
+			</div>
+			<AppPieChart />
+			<div className="bg-primary-foreground p-4 rounded-lg">
+				<TodoList />
+			</div>
+			<div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+				<AppAreaChart />
+			</div>
+		</div>
 	);
-}
+};
+
+export default Homepage;
